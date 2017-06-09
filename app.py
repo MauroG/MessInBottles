2a8cff3008b61c31c028d4877237c76e7b0ea24e
Mauro Gardenal <mauro.gardenal@gmail.com>
2017-06-09 19:01:32 +0200

    Code reformat + improved italian language

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, send_from_directory, request, abort
 import os.path
 app = Flask(__name__)
 
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+# wsgi_app = app.wsgi_app # It's useful in any way?
+
+
 @app.route('/api/<path:path>', methods = ["GET", "POST"])
 def api(path=None):
 	if request.method == "POST":
@@ -10,6 +15,7 @@ def api(path=None):
 	if request.method == "GET":
 		print('it works')
 	abort(400)
+
 
 @app.route('/')
 @app.route('/<path:path>')
@@ -21,3 +27,7 @@ def sendPureHTML(path=None):
 		return send_from_directory('templates', path)
 	else:
 		return send_from_directory('templates', 'index.html')
+
+# Uncomment the 2 lines below to run this "app.py" and avoid to use bootstrap.bat or equivalent bootstrap.sh
+# if __name__ == '__main__':
+#   app.run(port=5000, debug=True)
